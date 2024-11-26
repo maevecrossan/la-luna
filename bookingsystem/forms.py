bd389b67c9e859bb5626541811f84448916656de
Maeve Crossan <maevecrossan@gmail.com>
2024-11-26 08:52:47 +0000

    Grey-out days in the past on booking form date input

--- bookingsystem/forms.py
+++ bookingsystem/forms.py
@@ -1,3 +1,4 @@
+from datetime import date
 from .models import Booking
 from django import forms
 
@@ -12,8 +13,19 @@ class BookingForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'placeholder': 'Full Name', 'class': 'formbold-form-input'}),
             'phone_number': forms.TextInput(attrs={'placeholder': 'Phone Number', 'class': 'formbold-form-input'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Email Address', 'class': 'formbold-form-input'}),
-            'date': forms.DateInput(attrs={'type': 'date', 'class': 'formbold-form-input'}),
+            'date': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'formbold-form-input',
+                }
+            ),
             'time': forms.Select(attrs={'class': 'formbold-form-input'}),
             'guests': forms.Select(attrs={'class': 'formbold-form-input'}),
             }
         
+    def __init__(self, *args, **kwargs):
+        """
+        Prevents bookings in the past.
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['date'].widget.attrs['min'] = date.today().strftime('%Y-%m-%d')
--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Booking
 from .forms import BookingForm
+from datetime import date
 
 
 # Create your views here.
 def booking_system(request):
     form = BookingForm()
-    return render(request, '../templates/bookings.html', {'form': form})
+    today = date.today().strftime('%Y-%m-%d')
+    return render(request, '../templates/bookings.html', {'form': form, 'today': today})
