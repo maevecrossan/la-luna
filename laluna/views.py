d28f8f55848f9c69b2e842bb202a523a6e924a56
Maeve Crossan <maevecrossan@gmail.com>
2024-12-11 13:37:22 +0000

    Remove contact view from app-level views.

--- laluna/views.py
+++ laluna/views.py
@@ -1,13 +1,19 @@
+"""
+Defines project-level views.
+"""
+
 from django.shortcuts import render
 
 
 def index(request):
+    """
+    View for index.html page.
+    """
     return render(request, '../templates/index.html')
 
 
 def menu(request):
+    """
+    View for menu.html page.
+    """
     return render(request, '../templates/menu.html')
-
-
-def contact(request):
-    return render(request, '../templates/contact.html')
