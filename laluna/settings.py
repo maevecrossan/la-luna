151417180ecc507dbd221038547ad5bd3403e59d
Maeve Crossan <maevecrossan@gmail.com>
2024-12-11 13:31:29 +0000

    Adjust settings file to comply to PEP8 standards where possible.

--- laluna/settings.py
+++ laluna/settings.py
@@ -31,8 +31,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-maevecrossan-laluna-3dgn3xzyd81.ws.codeinstitute-ide.net',
-                 '.herokuapp.com']
+ALLOWED_HOSTS = [
+            '8000-maevecrossan-laluna-3dgn3xzyd81.ws.codeinstitute-ide.net',
+            '.herokuapp.com'
+            ]
 
 
 # Application definition
