d1d76c9a6071fa926ddda1faa2f074806a66dc23
Maeve Crossan <maevecrossan@gmail.com>
2024-12-11 13:56:37 +0000

    Add docstring and extra line to be PEP8 compliant.

--- contactsystem/admin.py
+++ contactsystem/admin.py
@@ -1,14 +1,22 @@
+"""
+Register app-level models.
+"""
+
 from django.contrib import admin
 from .models import Contact
 from .forms import ContactForm
+
 
 class ContactAdmin(admin.ModelAdmin):
     """
     Set parameters for admin page.
     """
     form = ContactForm
-    list_display = ('contact_name', 'contact_phone_number', 'contact_email', 'contact_message')
+    list_display = ('contact_name', 'contact_phone_number',
+                    'contact_email', 'contact_message')
+
 
 # Register your models here.
+
 
 admin.site.register(Contact, ContactAdmin)
