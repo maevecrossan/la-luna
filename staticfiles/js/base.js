c77c6960c3484dbb4d95800e84fd8cf5e64e791e
Maeve Crossan <maevecrossan@gmail.com>
2024-12-12 08:50:24 +0000

    Collect and update static files.

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,6 +1,7 @@
 // Contains function linked solely to base.html
 
-/* Toggle between showing and hiding the mobile menu links when the user clicks on the hamburger menu / bar icon */
+/* Toggle between showing and hiding the mobile menu links 
+when the user clicks on the hamburger menu / bar icon */
 document.addEventListener("DOMContentLoaded", function() {
     // Function to toggle mobile menu
     function displayMobileMenu() {
