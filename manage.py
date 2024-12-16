424446bd9756be9890a00556503e1bcd009ef8ce
Maeve Crossan <maevecrossan@gmail.com>
2024-12-16 21:03:01 +0000

    Remove remaining traces Flask. Adjust email html to prepopulate is user is signed in.

--- manage.py
+++ manage.py
@@ -2,8 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import json
-from flask import Flask, render_template, request, flash
 
 
 def main():
@@ -20,36 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
-
-
 if __name__ == '__main__':
     main()
-
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
-@app.route("/menu")
-def menu(): # view is called inside html file
-    return render_template("menu.html", page_title="Menu")
-
-
-@app.route("/bookings")
-def bookings(): # view is called inside html file
-    return render_template("bookings.html", page_title="Bookings")
-
-
-@app.route("/bookings", methods=['POST'])
-def handle_booking():
-    # Handle the booking here
-    return 'Booking received!'
-
-
-@app.route("/contact")
-def contact(): # view is called inside html file
-    return render_template("contact.html", page_title="Contact Us")
-
-
