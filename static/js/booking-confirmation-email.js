0269ffb4ed9a3caf99158b7571e54888ef9a8679
Maeve Crossan <maevecrossan@gmail.com>
2024-12-17 12:54:52 +0000

    Update email.js function to only send if all form fields are valid.

--- static/js/booking-confirmation-email.js
+++ static/js/booking-confirmation-email.js
@@ -1,20 +1,39 @@
 // CRED: CI UCD Resume Template
- function sendBookingConfirmation(contactForm) {
-     emailjs.send("service_3cz29sk", "template_j6lagrm", {
-         "name" :contactForm.name.value,
-         "phone_number": contactForm.phone_number.value,
-         "email": contactForm.email.value,
-         "date": contactForm.date.value,
-         "time": contactForm.time.value,
-         "guests": contactForm.guests.value
-     })
-     .then(
-         function(response) {
-             console.log("SUCCESS", response);
-         },
-         function(error) {
-             console.log("FAILED", error);
-         }
-     );
-     return true;
- }
+function sendBookingConfirmation(contactForm) {
+    if (form.checkValidity()) {
+        // Get all input elements within the form
+        var inputs = form.querySelectorAll('input, textarea, select');
+
+        // Object to hold form data
+        var formData = {};
+        var isValid = true;
+
+        // Check each input field
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            if (input.required && !input.value) {
+                alert("Please fill out the " + input.name + " field.");
+                isValid = false;
+                break;
+            }
+            formData[input.name] = input.value;
+        }
+
+        if (isValid) {
+            emailjs.sendForm("service_3cz29sk", "template_j6lagrm", formData)
+                .then(
+                    function (response) {
+                        console.log('Email sent successfully:', response);
+                        form.reset();
+                    },
+                    function (error) {
+                        console.log('Failed to send email:', error);
+                    }
+                );
+            return true;
+        } else {
+            form.reportValidity();
+            return false;
+        }
+    }
+}
