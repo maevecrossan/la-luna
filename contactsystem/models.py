28c9157e176ba5336c216e063c1a327c0f76320a
Maeve Crossan <maevecrossan@gmail.com>
2024-12-19 08:29:57 +0000

    Add custom phone validator from booking model to contact model.

--- contactsystem/models.py
+++ contactsystem/models.py
@@ -2,8 +2,19 @@
 
 from django.db import models
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+# Custom validator for phone number minimum length
+def validate_min_length(value):
+    """Ensure the number has at least 10 digits"""
+
+    if len(value.replace("+", "").replace(" ", "")) < 9:
+        raise ValidationError(
+            "Phone number must have between 9 and 15 digits."
+        )
 
 
 class Contact(models.Model):
