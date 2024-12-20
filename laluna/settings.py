08837503d7d98c260799b540e554267fdba4fbc5
Maeve Crossan <maevecrossan@gmail.com>
2024-12-20 16:19:12 +0000

    Remove env import error and upload pep8 screenshot.

--- laluna/settings.py
+++ laluna/settings.py
@@ -15,7 +15,8 @@ import os
 import dj_database_url
 
 if os.path.isfile('env.py'):
-    import env  # pylint: disable=unused-import
+    import env
+    _ = env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
